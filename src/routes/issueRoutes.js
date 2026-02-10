743ec47091091b45fab41310b4f0485c480875fd
Victor Rydberg <victorydberg@gmail.com>
2026-02-10 15:22:20 +0100

    mount validator middleware to routes and add repo route

--- src/routes/issueRoutes.js
+++ src/routes/issueRoutes.js
@@ -1,22 +1,31 @@
 import express from 'express'
 import * as issueController from '../controllers/issueController.js'
+import {
+  validateIssueId,
+  validateIssueCreate,
+  validateComment,
+  validateStatusUpdate,
+  validateExplorerQuery
+} from '../middleware/validators.js'
 
 const router = express.Router()
 
-router.get('/', issueController.fetchExplorerIssues)
+router.get('/', validateExplorerQuery, issueController.fetchExplorerIssues)
 
-router.get('/expanded/:iid', issueController.expandIssue)
+router.get('/expanded/:iid', validateIssueId, issueController.expandIssue)
 
-router.put('/update-issue-status/:iid', issueController.updateIssueStatus)
+router.put('/update-issue-status/:iid', validateIssueId, validateStatusUpdate, issueController.updateIssueStatus)
 
-router.post('/add-comment/:iid', issueController.addIssueComment)
+router.post('/add-comment/:iid', validateIssueId, validateComment, issueController.addIssueComment)
 
 router.get('/create', issueController.renderIssueCreation)
 
-router.post('/create', issueController.createIssue)
+router.post('/create', validateIssueCreate, issueController.createIssue)
 
-router.get('/edit/:iid', issueController.renderIssueEdit)
+router.get('/edit/:iid', validateIssueId, issueController.renderIssueEdit)
 
-router.put('/edit/:iid', issueController.editIssue)
+router.put('/edit/:iid', validateIssueId, validateIssueCreate, issueController.editIssue)
+
+router.get('/repository', issueController.renderRepository)
 
 export default router
